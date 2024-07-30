f4863ae01cc8427b52d31a637cd8982bedc46c38
G M <propbono@gmail.com>
2024-07-30 17:43:02 -0400

    feat: update my last year responses using new alghoritm

---
+++ main.test.ts
@@ -0,0 +1,87 @@
+import { describe, expect, it } from 'vitest';
+import { filterTwoSequences, getPossiblePrimeNumberOccurences } from './main';
+
+describe('Project evaluation criteria', () => {
+  it('should contain all elements from sequence A (maintaining the order) except those, that are present in sequence B p times, where p is a prime number', () => {
+    const sequenceA = [2, 3, 9, 2, 5, 1, 3, 7, 10];
+    const sequenceB = [2, 1, 3, 4, 3, 10, 6, 6, 1, 7, 10, 10, 10];
+    const expected = [2, 9, 2, 5, 7, 10];
+
+    const result = filterTwoSequences(sequenceA, sequenceB);
+
+    expect(expected).toEqual(result);
+  });
+
+  it('should return an empty array if sequence A is empty', () => {
+    const sequenceA: number[] = [];
+    const sequenceB = [2, 1, 3, 4, 3, 10, 6, 6, 1, 7, 10, 10, 10];
+
+    const expected: number[] = [];
+
+    const result = filterTwoSequences(sequenceA, sequenceB);
+
+    expect(expected).toEqual(result);
+  });
+
+  it('should return an sequence A array if sequence B is empty', () => {
+    const sequenceA = [2, 3, 9, 2, 5, 1, 3, 7, 10];
+    const sequenceB: number[] = [];
+
+    const result = filterTwoSequences(sequenceA, sequenceB);
+
+    expect(sequenceA).toEqual(result);
+  });
+
+  it('should return an empty array if both sequence A and sequence B are empty', () => {
+    const sequenceA: number[] = [];
+    const sequenceB: number[] = [];
+
+    const expected: number[] = [];
+
+    const result = filterTwoSequences(sequenceA, sequenceB);
+
+    expect(expected).toEqual(result);
+  });
+});
+describe('getPossiblePrimeNumberOccurences - Find Prime Numbers Up to n max length of array', () => {
+  it('should return an empty array if Sequence B is empty', () => {
+    const sequence = [];
+    const expected: Number[] = [];
+
+    const result = getPossiblePrimeNumberOccurences(sequence.length);
+
+    expect(expected).toMatchObject(result);
+  });
+
+  it('should return an empty array if Sequence B has only one element', () => {
+    const sequence = [1];
+    const expected: Number[] = [];
+
+    const result = getPossiblePrimeNumberOccurences(sequence.length);
+
+    expect(expected).toMatchObject(result);
+  });
+
+  it('should return an array of prime numbers calculated based on Sequence B length', () => {
+    const sequence1 = [1, 2];
+    const expected1: Number[] = [2];
+
+    const result1 = getPossiblePrimeNumberOccurences(sequence1.length);
+
+    expect(expected1).toMatchObject(result1);
+
+    const sequence2 = [1, 2, 3, 4, 5, 6];
+    const expected2: Number[] = [2, 3, 5];
+
+    const result2 = getPossiblePrimeNumberOccurences(sequence2.length);
+
+    expect(expected2).toMatchObject(result2);
+
+    const sequence3 = [4, 6, 2, 5, 6, 7, 8, 0];
+    const expected3: Number[] = [2, 3, 5, 7];
+
+    const result3 = getPossiblePrimeNumberOccurences(sequence3.length);
+
+    expect(expected3).toMatchObject(result3);
+  });
+});
